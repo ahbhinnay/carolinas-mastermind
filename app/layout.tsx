cf63df91ae0bf84f513e7310cf5d69e687fa7508
wesco-abhinaydonadula <abhinay@reaibrain.com>
2025-01-14 09:31:29 -0500

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Carolinas Multifamily Real Estate Summit 2025 | Hosted by Barry Coppedge",
+  title: "Carolinas Mastermind, Wilmington, NC | Hosted by Barry Coppedge",
   description: "Connecting developers and investors for unparalleled growth | Hosted by Barry",
 };
 
